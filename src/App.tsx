a8c0788f580da13a5aa66a6715862bb1f3662170
Pyunghyuk Yoo <yoophi@gmail.com>
2022-10-25 18:00:56 +0900

    ✨ ADD: posts list page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import routerProvider from "@pankod/refine-react-router-v6";
 import dataProvider from "@pankod/refine-simple-rest";
 import { Layout } from "./components/Layout";
 import { PostIcon } from "./icons";
+import { PostList } from "./pages";
 
 const App: React.FC = () => {
   return (
     <Refine
       routerProvider={routerProvider}
       dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-      resources={[{ name: "posts", icon: PostIcon }]}
+      resources={[{ name: "posts", icon: PostIcon, list: PostList }]}
       Layout={Layout}
     />
   );
