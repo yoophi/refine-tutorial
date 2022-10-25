77a6252d7ffe68eeb81afc9112df8e9f206afe9e
Pyunghyuk Yoo <yoophi@gmail.com>
2022-10-25 17:51:00 +0900

    ✨ ADD: resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Refine } from "@pankod/refine-core";
 import routerProvider from "@pankod/refine-react-router-v6";
 import dataProvider from "@pankod/refine-simple-rest";
+import { PostIcon } from "./icons";
 
 const App: React.FC = () => {
   return (
     <Refine
       routerProvider={routerProvider}
       dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      resources={[{ name: "posts", icon: PostIcon }]}
     />
   );
 };
